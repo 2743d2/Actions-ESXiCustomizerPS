3d50cd0af465a530b8804cc5777f3f69f280153d
CCA2878 <1054331608@qq.com>
2022-03-02 16:18:41 +0800

    2022-3-2_16:18:24_UseChoco

--- config.ps1
+++ config.ps1
@@ -1,6 +1,1 @@
 Set-ExecutionPolicy -ExecutionPolicy Bypass
-Get-ExecutionPolicy -List
-Set-PSRepository PSGallery -InstallationPolicy Trusted
-Install-Module -Name VMware.PowerCLI -Scope CurrentUser
-Get-ExecutionPolicy -List
-Set-PowerCLIConfiguration -InvalidCertificateAction Ignore
