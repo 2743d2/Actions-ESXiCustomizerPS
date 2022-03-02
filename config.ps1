afccef50f395e2f743628404992c4480617e3a3b
CCA2878 <1054331608@qq.com>
2022-03-02 15:07:45 +0800

    2022-3-2_15:07:30_DebugCode

--- config.ps1
+++ config.ps1
@@ -1,2 +1,10 @@
 Set-ExecutionPolicy -ExecutionPolicy Bypass
+echo 00
 Get-ExecutionPolicy -List
+echo 01
+Set-PSRepository PSGallery -InstallationPolicy Trusted
+Install-Module VMware.PowerCLI
+echo 10
+Get-ExecutionPolicy -List
+echo 11
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore
