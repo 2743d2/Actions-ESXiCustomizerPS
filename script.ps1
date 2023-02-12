803b27463e87ec79363a3d7d07b4eeccdab08eaf
2743d2 <17898638+2743d2@users.noreply.github.com>
2023-02-12 14:07:30 +0800

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,1 +1,1 @@
-.\ESXi-Customizer-PS.ps1 -v67  -vft -load net55-r8168 -nsc -ipname ESXi-6.7.0-20220704001-standard -ipdesc OverrideLatest -ipvendor VMware
+.\ESXi-Customizer-PS.ps1 -v67  -vft -load net55-r8168 -nsc
